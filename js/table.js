254c1e3798338186e4b8994ce5680df3b26a9abc
TheKipmaster <fkipman@hotmail.com>
2017-11-20 19:17:57 -0200

    Adiciona colunas de reação e concentração à tabela

--- js/table.js
+++ js/table.js
@@ -8,7 +8,13 @@ new_row_button.addEventListener("click", function () {
 
   new_row.id = (table.children.length+1);
   $.each(new_row.children, function(i, td) {
-    td.children[0].value = "";
+    if (td.children[0].name == "concentration") {
+      td.children[0].checked = false;
+    } else if (td.children[0].name == "reaction") {
+      td.children[0].checked = true;      
+    } else {
+      td.children[0].value = "";
+    }
   });
 
   table.appendChild(new_row);
